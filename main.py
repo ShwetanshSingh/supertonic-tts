33dabc0c548820c6207f22425d585313ac4b4815
Shwetansh Singh <contactsinghshwetansh@gmail.com>
2026-05-27 17:48:21 +0530

    fix(main.py): update lang

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     style = tts.get_voice_style(voice_name="M1")
 
     text = "आज के समय में तकनीक हमारे जीवन का एक अहम हिस्सा बन चुकी है। सुबह उठने से लेकर रात को सोने तक, हम किसी न किसी रूप में तकनीक का उपयोग करते हैं। स्मार्टफोन, इंटरनेट और कंप्यूटर ने हमारे काम करने के तरीके को पूरी तरह बदल दिया है।"
-    wav, duration = tts.synthesize(text, voice_style=style, lang="en")
+    wav, duration = tts.synthesize(text, voice_style=style, lang="hin")
 
     tts.save_audio(wav, "output-hin.wav")
     print(f"Generated {duration}s of audio")
